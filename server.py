f8f2a28dc05631533e580a006bcf10a160f3c126
Alexander Petrov <petrov.aa@phystech.edu>
2023-11-12 09:35:31 +0400

    remove extra server code

--- server.py
+++ server.py
@@ -2,13 +2,9 @@ import numpy as np
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from tensorflow import keras
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 CORS(app)  # Включаем CORS для всего приложения
-
-# Переменная для хранения данных
-stored_data = None
 
 model = keras.saving.load_model('model.keras')
 
@@ -18,22 +14,14 @@ def predict():
         # Получаем JSON-массив из тела запроса
         data = request.get_json()
 
-        # Сохраняем данные в переменной
-        global stored_data
-        stored_data = data
-
         input = np.array(data)
-
         input = input.reshape((1, 28, 28))
-
-        print(input.shape)
-
         predict = model.predict(input).tolist()
 
         number = np.argmax(predict)
 
         # Возвращаем успешный ответ
-        return jsonify({'number': int(number), 'predict': predict})
+        return jsonify({'number': number, 'predict': predict})
     except Exception as e:
         # В случае ошибки возвращаем сообщение об ошибке
         return jsonify({'status': 'error', 'message': str(e)})
@@ -41,4 +29,4 @@ def predict():
 
 if __name__ == '__main__':
     # Запуск сервера на порте 5000
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
